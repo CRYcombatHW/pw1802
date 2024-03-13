beb575c4ca926b566e9ab473d7c088b0d9033ae0
CRYcombat <koteika5063@gmail.com>
2024-03-13 13:52:15 +0400

    Second task

---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+
+namespace Task2
+{
+	internal class Program
+	{
+		static void Main(string[] args) {
+			string connectionStr = @"Data Source=CRYCOMBATPC\MSSQL2019;Integrated Security=True;Connect Timeout=30;";
+			using SqlConnection connection = new SqlConnection(connectionStr);
+
+			Console.WriteLine("Connecting...");
+			try {
+				connection.Open();
+			}
+			catch (Exception x) {
+				Console.WriteLine("Cant connect");
+				return;
+			}
+			Console.WriteLine("Connected to server");
+
+			try {
+				new SqlCommand("USE Storehouse", connection).ExecuteNonQuery();
+			}
+			catch {
+				Console.WriteLine("Cant connect to Storehouse database");
+				return;
+			}
+
+			Console.WriteLine("Connected to Storehouse database");
+		}
+	}
+}
